b4e6643e2b8d82146216bf4c1b35e9caf1880d67
Robin Harper <cabrel@zerklabs.com>
2014-06-11 14:32:58 -0400

    Moved influxdb config to flags Moved all communications to channels

--- latencylog.go
+++ latencylog.go
@@ -1,32 +1,28 @@
 package main
 
 import (
-	// "bytes"
 	"crypto/tls"
-	// "encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/influxdb/influxdb-go"
+	h "github.com/zerklabs/gohelpers"
 )
 
 type Snapshot struct {
-	Duration         float64   `json:"duration"`
-	DurationFormat   string    `json:"durationformat"`
-	DistanceEstimate string    `json:"distanceestimate"`
-	Status           int       `json:"status"`
-	End              time.Time `json:"end"`
-	Host             string    `json:"host"`
-	Port             int       `json:"port"`
-	Start            time.Time `json:"start"`
-	LocalIP          string    `json:"localip"`
-	LocalPort        string    `json:"localport"`
+	Duration  float64   `json:"duration"`
+	Status    int       `json:"status"`
+	End       time.Time `json:"end"`
+	Host      string    `json:"host"`
+	Port      int       `json:"port"`
+	Start     time.Time `json:"start"`
+	LocalIP   string    `json:"localip"`
+	LocalPort int       `json:"localport"`
+	Proto     string    `json:"proto"`
 }
 
 var (
@@ -41,30 +37,61 @@ var (
 
 func main() {
 	var (
-		tcpAddress = flag.String("tcp-address", "", "Target IP:PORT to connect to")
-		duration   = flag.Int("duration", 1, "How long to run for (minutes)")
-		// httpAddress = flag.String("http-address", "", "The target HTTP server")
-		// httpPath    = flag.String("http-path", "/put", "The URI path for the NSQ publisher")
-		// httpSSL     = flag.Bool("use-https", false, "Use HTTPS?")
-
-		err error
+		tcpAddress       = flag.String("tcp-address", "", "Target IP:PORT to connect to")
+		influxdbAddress  = flag.String("influxdb-address", "", "InfluxDB Host")
+		influxDatabase   = flag.String("influxdb-database", "", "InfluxDB Database")
+		influxdbUsername = flag.String("influxdb-username", "", "InfluxDB Username")
+		influxdbPassword = flag.String("influxdb-password", "", "InfluxDB Password")
+		duration         = flag.Int("duration", 1, "How long to run for (minutes)")
+		err              error
 	)
 
 	flag.Parse()
 
 	if len(*tcpAddress) == 0 {
-		fmt.Println("IP:PORT required")
+		fmt.Println("TCP address required")
 
 		flag.PrintDefaults()
 		return
 	}
 
+	if len(*influxdbAddress) == 0 {
+		fmt.Println("InfluxDB Host required")
+		flag.PrintDefaults()
+		return
+	}
+
+	if len(*influxDatabase) == 0 {
+		fmt.Println("InfluxDB Database required")
+		flag.PrintDefaults()
+		return
+	}
+
+	if len(*influxdbUsername) == 0 {
+		fmt.Println("InfluxDB Username required")
+		flag.PrintDefaults()
+		return
+	}
+
+	if len(*influxdbPassword) == 0 {
+		fmt.Println("InfluxDB Password required")
+		flag.PrintDefaults()
+		return
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client = &http.Client{Transport: tr}
+
 	influxConfig := &influxdb.ClientConfig{
-		Host:     "api.cobhamna.com/influxdb",
-		Database: "latencylog-metrics",
-		Username: "latencylog",
-		Password: "haej0Zoh",
-		IsSecure: true,
+		Host:       *influxdbAddress,
+		Database:   *influxDatabase,
+		Username:   *influxdbUsername,
+		Password:   *influxdbPassword,
+		IsSecure:   true,
+		HttpClient: client,
 	}
 
 	db, err = influxdb.NewClient(influxConfig)
@@ -73,83 +100,30 @@ func main() {
 		panic(err)
 	}
 
-	// if len(*httpAddress) > 0 {
-	// 	useHttp = true
-	// 	httpUri = fmt.Sprintf("%s%s", *httpAddress, *httpPath)
-
-	// 	if *httpSSL {
-	// 		httpUri = "https://" + httpUri
-	// 	} else {
-	// 		httpUri = "http://" + httpUri
-	// 	}
-	// }
+	runFor, _ := time.ParseDuration(fmt.Sprintf("%dm", *duration))
+	log.Printf("Running for: %s", runFor)
 
 	snapshots = make([]*Snapshot, 0)
+	resChan := make(chan *Snapshot)
 
 	tickChan := time.NewTicker(time.Second * 1)
+	timeChan := time.NewTimer(runFor).C
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	client = &http.Client{Transport: tr}
-
-	go func() {
-		for _ = range tickChan.C {
-			host := strings.Split(*tcpAddress, ":")[0]
-			port, _ := strconv.Atoi(strings.Split(*tcpAddress, ":")[1])
-
-			res, err := run(host, port)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
+	for {
+		select {
+		case <-tickChan.C:
+			host, port := h.ParseNetAddress(*tcpAddress)
+			go runTCP(host, port, resChan)
+		case res := <-resChan:
 			log.Printf("%v, %s:%d", res.Duration, res.Host, res.Port)
-
 			snapshots = append(snapshots, res)
+		case <-timeChan:
+			tickChan.Stop()
+			aggregate(snapshots, *duration)
+			return
 		}
-	}()
-
-	sleepFor, _ := time.ParseDuration(fmt.Sprintf("%dm", *duration))
-	log.Printf("Running for: %s", sleepFor)
-	time.Sleep(sleepFor)
-	tickChan.Stop()
-
-	aggregate(snapshots, *duration)
+	}
 }
-
-// func record(snapshot *Snapshot) {
-// 	// if useHttp {
-// 	// 	b, err := json.Marshal(snapshot)
-
-// 	// 	if err != nil {
-// 	// 		log.Fatal(err)
-// 	// 	}
-
-// 	// 	buf := bytes.NewBufferString(string(b))
-// 	// 	_, err = client.Post(httpUri, "application/json", buf)
-
-// 	// 	if err != nil {
-// 	// 		log.Printf("HTTP Error: %s", err)
-// 	// 	}
-// 	// }
-
-// 	series := []*influxdb.Series{
-// 		&influxdb.Series{
-// 			Name:    "measurements",
-// 			Columns: []string{"source", "destination", "duration", "status"},
-// 			Points: [][]interface{}{
-// 				{fmt.Sprintf("%s:%s", snapshot.LocalIP, snapshot.LocalPort), fmt.Sprintf("%s:%d", snapshot.Host, snapshot.Port), snapshot.Duration, snapshot.Status},
-// 			},
-// 		},
-// 	}
-
-// 	err := db.WriteSeries(series)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
 
 func aggregate(snapshots []*Snapshot, duration int) {
 	if len(snapshots) > 0 {
@@ -161,10 +135,8 @@ func aggregate(snapshots []*Snapshot, duration int) {
 			total += v.Duration
 			series = append(series, &influxdb.Series{
 				Name:    "measurements",
-				Columns: []string{"source", "destination", "duration", "status", "start", "end"},
-				Points: [][]interface{}{
-					{fmt.Sprintf("%s:%s", v.LocalIP, v.LocalPort), fmt.Sprintf("%s:%d", v.Host, v.Port), v.Duration, v.Status, v.Start, v.End},
-				},
+				Columns: []string{"source", "sourceport", "destination", "destinationport", "duration", "status", "start", "end", "proto"},
+				Points:  [][]interface{}{{v.LocalIP, v.LocalPort, v.Host, v.Port, v.Duration, v.Status, v.Start, v.End, v.Proto}},
 			})
 		}
 
@@ -178,57 +150,49 @@ func aggregate(snapshots []*Snapshot, duration int) {
 	}
 }
 
-func run(host string, port int) (*Snapshot, error) {
+func runTCP(host string, port int, resChan chan *Snapshot) {
 	snap := &Snapshot{}
 
-	snap.DurationFormat = "ms"
 	snap.Host = host
 	snap.Port = port
+	snap.Proto = "TCP"
 
 	snap.Start = time.Now()
 	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
 
 	if err != nil {
 		snap.Status = 0
+		snap.Duration = float64(0)
+		snap.LocalIP, snap.LocalPort = h.ParseNetAddress(c.LocalAddr().String())
 		snap.End = time.Now()
-		return snap, err
+
+		resChan <- snap
+		return
 	}
 
-	c.Write(make([]byte, 8))
+	_, err = c.Write(make([]byte, 8))
+
+	if err != nil {
+		snap.LocalIP, snap.LocalPort = h.ParseNetAddress(c.LocalAddr().String())
+		snap.End = time.Now()
+
+		resChan <- snap
+		return
+	}
 
 	snap.End = time.Now()
 	dur := snap.End.Sub(snap.Start)
 
 	snap.Duration = dur.Seconds() * 1e3
-	snap.LocalIP = strings.Split(c.LocalAddr().String(), ":")[0]
-	snap.LocalPort = strings.Split(c.LocalAddr().String(), ":")[1]
+	snap.LocalIP, snap.LocalPort = h.ParseNetAddress(c.LocalAddr().String())
 	snap.Status = 1
 
-	if snap.Duration <= float64(10) {
-		snap.DistanceEstimate = "sub10ms"
-	} else if snap.Duration <= float64(20) {
-		snap.DistanceEstimate = "sub20ms"
-	} else if snap.Duration <= float64(30) {
-		snap.DistanceEstimate = "sub30ms"
-	} else if snap.Duration <= float64(40) {
-		snap.DistanceEstimate = "sub40ms"
-	} else if snap.Duration <= float64(50) {
-		snap.DistanceEstimate = "sub50ms"
-	} else if snap.Duration <= float64(60) {
-		snap.DistanceEstimate = "sub60ms"
-	} else if snap.Duration <= float64(70) {
-		snap.DistanceEstimate = "sub70ms"
-	} else if snap.Duration <= float64(80) {
-		snap.DistanceEstimate = "sub80ms"
-	} else if snap.Duration <= float64(90) {
-		snap.DistanceEstimate = "sub90ms"
-	} else {
-		snap.DistanceEstimate = "above90ms"
-	}
-
 	if err := c.Close(); err != nil {
-		return snap, err
+		snap.Status = 0
+		resChan <- snap
+		return
 	}
 
-	return snap, nil
+	resChan <- snap
+	return
 }
