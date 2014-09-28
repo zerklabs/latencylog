cadc4d41cfb4024281c67d1cbd1b094720238074
Robin cabrel Harper <cabrel@zerklabs.com>
2014-09-28 01:59:37 -0400

    Added full support for getting web pages

--- latencylog.go
+++ latencylog.go
@@ -4,13 +4,15 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"log"
 	"net"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/influxdb/influxdb-go"
-	h "github.com/zerklabs/gohelpers"
+	"github.com/zerklabs/auburn/logging"
+	"github.com/zerklabs/auburn/utils"
 )
 
 type Snapshot struct {
@@ -32,6 +34,11 @@ var (
 
 	sourceSite string
 	destSite   string
+	log        = logging.Log
+	doTcp      bool
+	doWeb      bool
+	doDns      bool
+	offline    bool // if no influxdb address is given then TRUE
 )
 
 func main() {
@@ -39,6 +46,9 @@ func main() {
 
 	var (
 		tcpAddress       = flag.String("tcp-address", "", "Target IP:PORT to connect to")
+		webAddress       = flag.String("web-address", "", "Target URL to connect to")
+		dnsServerAddress = flag.String("dns-server-address", "", "Target DNS server to use")
+		resolveAddress   = flag.String("resolve-address", "", "Resource to resolve against the DNS server")
 		influxdbAddress  = flag.String("influxdb-address", "", "InfluxDB Host")
 		influxDatabase   = flag.String("influxdb-database", "", "InfluxDB Database")
 		influxdbUsername = flag.String("influxdb-username", "", "InfluxDB Username")
@@ -52,66 +62,96 @@ func main() {
 
 	flag.Parse()
 
-	if len(*tcpAddress) == 0 {
-		fmt.Println("TCP address required")
+	if len(*tcpAddress) == 0 && len(*webAddress) == 0 && len(*dnsServerAddress) == 0 {
+		fmt.Println("TCP, Web or DNS address required")
 
 		flag.PrintDefaults()
 		return
+	}
+
+	if len(*dnsServerAddress) > 0 && len(*resolveAddress) == 0 {
+		fmt.Println("DNS Address to resolve required")
+
+		flag.PrintDefaults()
+		return
+	}
+
+	if len(*tcpAddress) > 0 {
+		doTcp = true
+	} else if len(*webAddress) > 0 {
+		doWeb = true
+	} else if len(*dnsServerAddress) > 0 {
+		doDns = true
 	}
 
 	if len(*influxdbAddress) == 0 {
-		fmt.Println("InfluxDB Host required")
-		flag.PrintDefaults()
-		return
-	}
-
-	if len(*influxDatabase) == 0 {
-		fmt.Println("InfluxDB Database required")
-		flag.PrintDefaults()
-		return
-	}
-
-	if len(*influxdbUsername) == 0 {
-		fmt.Println("InfluxDB Username required")
-		flag.PrintDefaults()
-		return
-	}
-
-	if len(*influxdbPassword) == 0 {
-		fmt.Println("InfluxDB Password required")
-		flag.PrintDefaults()
-		return
-	}
-
-	sourceSite = *sourceLocation
-	destSite = *destLocation
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	if *useTLS {
-		client = &http.Client{Transport: tr}
+		fmt.Println("WARNING: Results will not be saved")
+		offline = true
+		//fmt.Println("InfluxDB Host required")
+		//flag.PrintDefaults()
+		//return
 	} else {
-		client = &http.Client{}
+		if len(*influxDatabase) == 0 {
+			fmt.Println("InfluxDB Database required")
+			flag.PrintDefaults()
+			return
+		}
+
+		if len(*influxdbUsername) == 0 {
+			fmt.Println("InfluxDB Username required")
+			flag.PrintDefaults()
+			return
+		}
+
+		if len(*influxdbPassword) == 0 {
+			fmt.Println("InfluxDB Password required")
+			flag.PrintDefaults()
+			return
+		}
+
+		if len(*sourceLocation) == 0 {
+			fmt.Println("Source location required")
+			flag.PrintDefaults()
+			return
+		}
+		sourceSite = *sourceLocation
+
+		if len(*destLocation) == 0 {
+			fmt.Println("Destination location required")
+			flag.PrintDefaults()
+			return
+		}
+		destSite = *destLocation
+
+		if !offline {
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+
+			if *useTLS {
+				client = &http.Client{Transport: tr}
+			} else {
+				client = &http.Client{}
+			}
+
+			influxConfig := &influxdb.ClientConfig{
+				Host:       *influxdbAddress,
+				Database:   *influxDatabase,
+				Username:   *influxdbUsername,
+				Password:   *influxdbPassword,
+				IsSecure:   *useTLS,
+				HttpClient: client,
+			}
+
+			db, err = influxdb.NewClient(influxConfig)
+			if err != nil {
+				log.Panic(err)
+				return
+			}
+		}
 	}
 
-	influxConfig := &influxdb.ClientConfig{
-		Host:       *influxdbAddress,
-		Database:   *influxDatabase,
-		Username:   *influxdbUsername,
-		Password:   *influxdbPassword,
-		IsSecure:   *useTLS,
-		HttpClient: client,
-	}
-
-	db, err = influxdb.NewClient(influxConfig)
-
-	if err != nil {
-		panic(err)
-	}
-
-	log.Printf("Running for: %s", *duration)
+	log.Infof("Running for: %s", *duration)
 
 	snapshots = make([]*Snapshot, 0)
 	resChan := make(chan *Snapshot)
@@ -122,10 +162,16 @@ func main() {
 	for {
 		select {
 		case <-tickChan.C:
-			host, port := h.ParseNetAddress(*tcpAddress)
-			go runTCP(host, port, resChan)
+			if doTcp {
+				host, port := utils.ParseNetAddress(*tcpAddress)
+				go runTCP(host, port, resChan)
+			} else if doWeb {
+				go runWeb(*webAddress, resChan)
+			} else if doDns {
+				go runDns(*dnsServerAddress, *resolveAddress, resChan)
+			}
 		case res := <-resChan:
-			log.Printf("%v, %s:%d", res.Duration, res.Host, res.Port)
+			log.Infof("%v, %s:%d", res.Duration, res.Host, res.Port)
 			snapshots = append(snapshots, res)
 		case <-timeChan:
 			tickChan.Stop()
@@ -136,28 +182,37 @@ func main() {
 }
 
 func aggregate(snapshots []*Snapshot, duration time.Duration) {
-	if len(snapshots) > 0 {
-		series := make([]*influxdb.Series, 0)
-		total := float64(0)
-		log.Printf("Snapshots: %d", len(snapshots))
+	if len(snapshots) == 0 {
+		return
+	}
 
-		for _, v := range snapshots {
-			total += v.Duration
+	series := make([]*influxdb.Series, 0)
+	total := float64(0)
+
+	log.Infof("Snapshots: %d", len(snapshots))
+
+	for _, v := range snapshots {
+		total += v.Duration
+		// don't bother creating a series element if we aren't connected
+		// to influxdb
+		if !offline {
 			series = append(series, &influxdb.Series{
-				Name:    fmt.Sprintf("%s_%s_latency", sourceSite, destSite),
+				Name:    fmt.Sprintf("%s.%s.latency", strings.ToUpper(sourceSite), destSite),
 				Columns: []string{"source", "destination", "destinationport", "duration", "status", "proto"},
 				Points:  [][]interface{}{{v.LocalIP, v.Host, v.Port, v.Duration, v.Status, v.Proto}},
 			})
 		}
+	}
 
-		avg := total / float64(len(snapshots))
-		log.Printf("Average after %s and %d checks: %vms", duration, len(snapshots), avg)
-
-		err := db.WriteSeries(series)
+	if len(series) > 0 {
+		err := db.WriteSeriesWithTimePrecision(series, influxdb.Microsecond)
 		if err != nil {
-			panic(err)
+			log.Error(err)
 		}
 	}
+
+	avg := total / float64(len(snapshots))
+	log.Infof("Average after %s and %d checks: %vms", duration, len(snapshots), avg)
 }
 
 func runTCP(host string, port int, resChan chan *Snapshot) {
@@ -171,7 +226,7 @@ func runTCP(host string, port int, resChan chan *Snapshot) {
 	snap.Start = time.Now()
 
 	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
-	snap.LocalIP, snap.LocalPort = h.ParseNetAddress(c.LocalAddr().String())
+	snap.LocalIP, snap.LocalPort = utils.ParseNetAddress(c.LocalAddr().String())
 
 	if err != nil {
 		snap.End = time.Now()
@@ -182,7 +237,7 @@ func runTCP(host string, port int, resChan chan *Snapshot) {
 	_, err = c.Write(make([]byte, 8))
 
 	if err != nil {
-		snap.LocalIP, snap.LocalPort = h.ParseNetAddress(c.LocalAddr().String())
+		snap.LocalIP, snap.LocalPort = utils.ParseNetAddress(c.LocalAddr().String())
 		snap.End = time.Now()
 
 		resChan <- snap
@@ -193,7 +248,7 @@ func runTCP(host string, port int, resChan chan *Snapshot) {
 	dur := snap.End.Sub(snap.Start)
 
 	snap.Duration = dur.Seconds() * 1e3
-	snap.LocalIP, snap.LocalPort = h.ParseNetAddress(c.LocalAddr().String())
+	snap.LocalIP, snap.LocalPort = utils.ParseNetAddress(c.LocalAddr().String())
 	snap.Status = 1
 
 	if err := c.Close(); err != nil {
@@ -204,4 +259,71 @@ func runTCP(host string, port int, resChan chan *Snapshot) {
 
 	resChan <- snap
 	return
+}
+
+func runWeb(address string, resChan chan *Snapshot) {
+	host := address
+	var port int
+	snap := &Snapshot{}
+	httpClient := http.Client{}
+
+	purl, err := url.Parse(address)
+	if err != nil {
+		log.Error(err)
+	}
+
+	_, port = utils.ParseNetAddress(purl.Host)
+	if strings.Contains(purl.Scheme, "https") {
+		// set the default https port if not present in the host address
+		if port == 0 {
+			port = 443
+		}
+	} else {
+		// set the default http port if not present in the host address
+		if port == 0 {
+			port = 80
+		}
+	}
+
+	snap.Host = host
+	snap.Port = port
+	snap.Proto = purl.Scheme
+	snap.Duration = float64(0)
+	snap.Status = 0
+	snap.Start = time.Now()
+
+	resp, err := httpClient.Get(address)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Error(err)
+		snap.End = time.Now()
+		resChan <- snap
+		return
+	}
+
+	// finalize the snapshot
+	snap.Status = 1
+	snap.End = time.Now()
+	dur := snap.End.Sub(snap.Start)
+	snap.Duration = dur.Seconds() * 1e3
+	snap.LocalIP = "127.0.0.1"
+	snap.LocalPort = 0
+	snap.Status = 1
+
+	resChan <- snap
+}
+
+func runDns(dnsServer string, query string, resChan chan *Snapshot) {
+	var host string
+	var port int
+	var snap Snapshot
+
+	host, port = utils.ParseNetAddress(dnsServer)
+
+	snap.Host = host
+	snap.Port = port
+	snap.Proto = "TCP"
+	snap.Duration = float64(0)
+	snap.Status = 0
+	snap.Start = time.Now()
 }
